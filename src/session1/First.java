7f5027f998706882c0dc10b2ed24643da9b2e37e
Bitaan-codes <lavanyasb006@gmail.com>
2025-08-30 12:48:01 +0530

    Update First.java

--- src/session1/First.java
+++ src/session1/First.java
@@ -2,6 +2,6 @@ package session1;
 
 public class First {
 	public static void main(String[] args) {
-		System.out.println("istudio session day1");
+		System.out.println("istudio session day1 begins today");
 	}
 }
